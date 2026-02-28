9005d6ec1646abfd5329fffb317d634af70a7681
Jan Thielert <janthielertjan@gmail.com>
2026-02-28 14:33:29 +0100

    Reader added

--- src/loganalyzer/Main.java
+++ src/loganalyzer/Main.java
@@ -1,7 +1,26 @@
 package loganalyzer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Log Analyzer started");
     }
+    
+    
+    public void readFromFile() {
+    	try(BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+    		String line;
+    		while((line = reader.readLine()) != null) {
+    			System.out.println(line);
+    		}
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    
+    
 }
