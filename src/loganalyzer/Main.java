3a16cdb33067c3a8e669b8951452588a867c7ead
Jan Thielert <janthielertjan@gmail.com>
2026-03-01 21:09:07 +0100

    Added count and print methods and revised the reader

--- src/loganalyzer/Main.java
+++ src/loganalyzer/Main.java
@@ -7,20 +7,44 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Log Analyzer started");
+        new Main().readFromFile("log.txt");
     }
     
+    private int infoCount = 0;
+    private int errorCount = 0;
+    private int warningCount = 0;
     
-    public void readFromFile() {
-    	try(BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+    public void readFromFile(String filename) {
+    	try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
     		String line;
     		while((line = reader.readLine()) != null) {
-    			System.out.println(line);
+    			processLine(line);
     		}
     	} catch(IOException e) {
     		e.printStackTrace();
     	}
+    	printSummary();
     }
     
+    private void processLine(String line) {
+    	if (line.contains("INFO")) {
+    		infoCount++;
+    	}
+    	
+    	if (line.contains("ERROR")) {
+    		errorCount++;
+    	}
+    	
+    	if (line.contains("WARNING")) {
+    		warningCount++;
+    	}
+    }
     
+    private void printSummary() {
+        System.out.println("\nLog Summary");
+        System.out.println("INFO: " + infoCount);
+        System.out.println("ERROR: " + errorCount);
+        System.out.println("WARNING: " + warningCount);
+    }
     
 }
